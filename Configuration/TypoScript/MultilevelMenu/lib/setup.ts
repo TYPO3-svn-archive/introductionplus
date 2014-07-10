7fd72e199d81ec667c85ace3124a429676cdc5e1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-10 21:15:10 +0000

    0.0.2 +++ #60253
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85874 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/MultilevelMenu/lib/setup.ts
+++ Configuration/TypoScript/MultilevelMenu/lib/setup.ts
@@ -4,18 +4,21 @@ lib.navigation {
       2 {
         IFSUB < lib.navigation.main.10.1.IFSUB
         IFSUB {
-          wrapItemAndSub  = <li class="dropdown XXdropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|</a>
           linkWrap        = <div class="glyphicon glyphicon-chevron-right" style="float:right"></div>|
+          wrapItemAndSub  = <li class="dropdown dropdown-submenu">|</li>
         }
         ACTIFSUB < lib.navigation.main.10.1.ACTIFSUB
         ACTIFSUB {
-          wrapItemAndSub = <li class="dropdown XXdropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|</a>
           linkWrap        = <div class="glyphicon glyphicon-chevron-right" style="float:right"></div>|
+          wrapItemAndSub  = <li class="dropdown dropdown-submenu">|</li>
         }
         CURIFSUB < lib.navigation.main.10.1.CURIFSUB
         CURIFSUB {
-          wrapItemAndSub = <li class="dropdown XXdropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|</a>
           linkWrap        = <div class="glyphicon glyphicon-chevron-right" style="float:right"></div>|
+          wrapItemAndSub  = <li class="dropdown dropdown-submenu">|</li>
         }
       }
       3 < .2
