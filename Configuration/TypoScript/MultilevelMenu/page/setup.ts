5d742b21f3d6c52e9031054b013aae429bfece87
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-10 20:19:24 +0000

    0.0.2 +++ #60253
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85862 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/MultilevelMenu/page/setup.ts
+++ Configuration/TypoScript/MultilevelMenu/page/setup.ts
@@ -1,6 +1,6 @@
 page {
   includeCSS {
-    introductionPlusMultilevelMenu = EXT:introductionplus/Resources/Public/Css/multilevelmenu.js
+    introductionPlusMultilevelMenu = EXT:introductionplus/Resources/Public/Css/multilevelmenu.css
   }
   includeJSFooterlibs {
     introductionPlusMultilevelMenu = EXT:introductionplus/Resources/Public/JavaScript/multilevelmenu.js
