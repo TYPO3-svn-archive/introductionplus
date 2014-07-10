c901400436aacf1258046f7d2b7e1ef1235c7fdd
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-10 23:53:53 +0000

    0.0.2 +++ #60255
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85882 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Header/',             'Introduction plus: Header');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/TopBar/',             'Introduction plus: TopBar');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/LanguageManager/',    'Introduction plus: language manager (1) en, de, ...');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/LanguageManager/de/', 'Introduction plus: language manager (1+) de, en, ...' );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/MultilevelMenu/',     'Introduction plus: multilevel menu');
