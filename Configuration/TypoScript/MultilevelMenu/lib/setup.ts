0d0c3e290bec2ab87c12bb379e83b66c1afefe58
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-10 20:42:39 +0000

    0.0.2 +++ #60253
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85867 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/MultilevelMenu/lib/setup.ts
+++ Configuration/TypoScript/MultilevelMenu/lib/setup.ts
@@ -4,15 +4,18 @@ lib.navigation {
       2 {
         IFSUB < lib.navigation.main.10.1.IFSUB
         IFSUB {
-          wrapItemAndSub = <li class="dropdown dropdown-submenu">|</li>
+          wrapItemAndSub  = <li class="dropdown dropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-chevron-right"></i>|</a>
         }
         ACTIFSUB < lib.navigation.main.10.1.ACTIFSUB
         ACTIFSUB {
           wrapItemAndSub = <li class="dropdown dropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-chevron-right"></i>|</a>
         }
         CURIFSUB < lib.navigation.main.10.1.CURIFSUB
         CURIFSUB {
           wrapItemAndSub = <li class="dropdown dropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-chevron-right"></i>|</a>
         }
       }
       3 < .2
