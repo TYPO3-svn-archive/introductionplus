747acc3ed6602db999c8b01b30477f55a32e143c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-10 21:01:30 +0000

    0.0.2 +++ #60253
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85871 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/MultilevelMenu/lib/setup.ts
+++ Configuration/TypoScript/MultilevelMenu/lib/setup.ts
@@ -4,18 +4,18 @@ lib.navigation {
       2 {
         IFSUB < lib.navigation.main.10.1.IFSUB
         IFSUB {
-          wrapItemAndSub  = <li class="dropdown dropdown-submenu">|<span class="glyphicon glyphicon-chevron-right"></span></li>
-          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a>
+          wrapItemAndSub  = <li class="dropdown dropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|</a> <span class="glyphicon glyphicon-chevron-right"></span>
         }
         ACTIFSUB < lib.navigation.main.10.1.ACTIFSUB
         ACTIFSUB {
-          wrapItemAndSub = <li class="dropdown dropdown-submenu">|<span class="glyphicon glyphicon-chevron-right"></span></li>
-          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a>
+          wrapItemAndSub = <li class="dropdown dropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|</a> <span class="glyphicon glyphicon-chevron-right"></span>
         }
         CURIFSUB < lib.navigation.main.10.1.CURIFSUB
         CURIFSUB {
-          wrapItemAndSub = <li class="dropdown dropdown-submenu">|<span class="glyphicon glyphicon-chevron-right"></span></li>
-          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|&nbsp;<span class="glyphicon glyphicon-chevron-right"></span></a>
+          wrapItemAndSub = <li class="dropdown dropdown-submenu">|</li>
+          allWrap         = <a href="#" class="dropdown-toggle" data-toggle="dropdown">|</a> <span class="glyphicon glyphicon-chevron-right"></span>
         }
       }
       3 < .2
