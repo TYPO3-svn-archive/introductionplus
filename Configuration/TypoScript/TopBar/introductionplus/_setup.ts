235f0d141e1866fbc4bb22a7159b4c1418528499
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-11 01:13:56 +0000

    0.0.2 +++ #60255
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85894 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/TopBar/introductionplus/_setup.ts
+++ Configuration/TypoScript/TopBar/introductionplus/_setup.ts
@@ -1,2 +1,3 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:introductionplus/Configuration/TypoScript/TopBar/introductionplus/bootstrap/setup.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:introductionplus/Configuration/TypoScript/TopBar/introductionplus/lib/setup.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:introductionplus/Configuration/TypoScript/TopBar/introductionplus/topbar/setup.ts">
