e4a99eb00a97b5f8992848bb5d36ae55333dfc4f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-11 09:44:51 +0000

    0.0.3 +++ #60265
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85919 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/LanguageMenu/lib/setup.ts
+++ Configuration/TypoScript/LanguageMenu/lib/setup.ts
@@ -4,55 +4,61 @@ introductionplus.lib {
     // language
   nav =
   nav {
-    language = HMENU
+      // text, icons
+    language =
     language {
-      special = language
-      special {
-        value = {$introductionplus.lib.nav.language.special}
-        normalWhenNoLanguage = 0
-      }
-      wrap =
-      1 = TMENU
-      1 {
-        noBlur = 1
-        NO = 1
-        NO {
-          linkWrap = <li>|</li>
-          doNotLinkIt = 1
-          stdWrap {
-            override = {$introductionplus.lib.nav.language.override}
-            typolink {
-              parameter {
-                data = page:uid
+      text = HMENU
+      text {
+        special = language
+        special {
+          value = {$introductionplus.lib.nav.language.special}
+          normalWhenNoLanguage = 0
+        }
+        wrap =
+        1 = TMENU
+        1 {
+          noBlur = 1
+          NO = 1
+          NO {
+            linkWrap = <li>|</li>
+            doNotLinkIt = 1
+            stdWrap {
+              override = {$introductionplus.lib.nav.language.override}
+              typolink {
+                parameter {
+                  data = page:uid
+                }
+                additionalParams = {$introductionplus.lib.nav.language.additionalParams}
+                addQueryString = 1
+                addQueryString {
+                  exclude = L,id,cHash,no_cache
+                  method = GET
+                }
+                useCacheHash = 1
+                no_cache = 0
               }
-              additionalParams = {$introductionplus.lib.nav.language.additionalParams}
-              addQueryString = 1
-              addQueryString {
-                exclude = L,id,cHash,no_cache
-                method = GET
-              }
-              useCacheHash = 1
-              no_cache = 0
             }
           }
+          ACT < .NO
+          ACT.linkWrap = <li class="active">|</li>
+          #### NO TRANSLATION AVAILABLE STATES ####
+          USERDEF1 < .NO
+          USERDEF1 {
+            linkWrap = <li class="text-muted">|</li>
+            stdWrap.typolink >
+          }
+          USERDEF2 < .ACT
+          USERDEF2 {
+            linkWrap = <li class="text-muted">|</li>
+            stdWrap.typolink >
+          }
         }
-        ACT < .NO
-        ACT.linkWrap = <li class="active">|</li>
-        #### NO TRANSLATION AVAILABLE STATES ####
-        USERDEF1 < .NO
-        USERDEF1 {
-          linkWrap = <li class="text-muted">|</li>
-          stdWrap.typolink >
-        }
-        USERDEF2 < .ACT
-        USERDEF2 {
-          linkWrap = <li class="text-muted">|</li>
-          stdWrap.typolink >
-        }
+        wrap = <ul id="language_menu" class="list-inline">|</ul>
       }
-      wrap = <ul id="language_menu" class="list-inline">|</ul>
+      icons < .text
     }
   }
 }
 
-tt_content.menu.20.introductionplusNavLanguage < introductionplus.lib.nav.language
+tt_content.menu.20.introductionplusNavLanguageText < introductionplus.lib.nav.language.text
+tt_content.menu.20.introductionplusNavLanguageIcons < introductionplus.lib.nav.language.icons
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
 t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:introductionplus/Configuration/PageTSConfig/Bootstrap/Components/pills.txt">');
-t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:introductionplus/Configuration/PageTSConfig/Nav/language.txt">');
+t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:introductionplus/Configuration/PageTSConfig/Nav/Language/text.txt">');
 
 ?>
