f399b1f107db54cbc7156037a32a2fa4618ac18a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-11 00:53:46 +0000

    0.0.2 +++ #60255
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85891 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/TopBar/introductionplus/topbar/setup.ts
@@ -0,0 +1,34 @@
+introductionplus {
+  topbar {
+    columns {
+      width {
+        left = TEXT
+        left {
+          value = {$introductionplus.topbar.columns.width.left}
+        }
+        center = TEXT
+        center {
+          value = {$introductionplus.topbar.columns.width.center}
+        }
+        right = TEXT
+        right {
+          value = {$introductionplus.topbar.columns.width.right}
+        }
+      }
+    }
+    folders {
+      left = TEXT
+      left {
+        value = {$introductionplus.topbar.folders.left}
+      }
+      center = TEXT
+      center {
+        value = {$introductionplus.topbar.folders.center}
+      }
+      right = TEXT
+      right {
+        value = {$introductionplus.topbar.folders.right}
+      }
+    }
+  }
+}
