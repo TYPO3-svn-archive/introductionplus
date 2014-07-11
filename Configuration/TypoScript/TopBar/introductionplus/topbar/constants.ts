50f21f9207932459725b0d78d84e42c1678724ec
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-11 01:43:45 +0000

    0.0.2 +++ #60255
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85900 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/TopBar/introductionplus/topbar/constants.ts
+++ Configuration/TypoScript/TopBar/introductionplus/topbar/constants.ts
@@ -2,11 +2,11 @@ introductionplus {
   topbar {
     columns {
       width {
-        # cat=introduction plus - top bar/250/100; type=options[1,2,3,4,5,6,7,8,9,10];   label=Width of left column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
+        # cat=introduction plus - top bar/250/100; type=options[0,1,2,3,4,5,6,7,8,9,10,11,12];   label=Width of left column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
         left    = 3
-        # cat=introduction plus - top bar/250/101; type=options[1,2,3,4,5,6,7,8,9,10];   label=Width of center column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
+        # cat=introduction plus - top bar/250/101; type=options[0,1,2,3,4,5,6,7,8,9,10,11,12];   label=Width of center column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
         center  = 6
-        # cat=introduction plus - top bar/250/102; type=options[1,2,3,4,5,6,7,8,9,10];   label=Width of right column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
+        # cat=introduction plus - top bar/250/102; type=options[0,1,2,3,4,5,6,7,8,9,10,11,12];   label=Width of right column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
         right   = 3
       }
     }
