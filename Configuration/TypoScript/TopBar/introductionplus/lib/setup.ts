f842c928c5c8d0b2b1c03df8b1236fcef855a1e5
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-11 01:00:40 +0000

    0.0.2 +++ #60255
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85893 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/TopBar/introductionplus/lib/setup.ts
+++ Configuration/TypoScript/TopBar/introductionplus/lib/setup.ts
@@ -3,15 +3,21 @@ introductionplus {
     folders {
       left < styles.content.get
       left {
-        pidInList = {$introductionplus.topbar.folders.left}
+        select {
+          pidInList = {$introductionplus.topbar.folders.left}
+        }
       }
       center < styles.content.get
       center {
-        pidInList = {$introductionplus.topbar.folders.center}
+        select {
+          pidInList = {$introductionplus.topbar.folders.center}
+        }
       }
       right < styles.content.get
       right {
-        pidInList = {$introductionplus.topbar.folders.right}
+        select {
+          pidInList = {$introductionplus.topbar.folders.right}
+        }
       }
     }
   }
