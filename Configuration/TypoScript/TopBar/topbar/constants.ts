e6084f006841cd545f0315e93524e2da33e49f06
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-11 00:18:34 +0000

    0.0.2 +++ #60255
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/introductionplus/trunk@85883 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/TopBar/topbar/constants.ts
@@ -0,0 +1,22 @@
+introductionplus {
+  topbar {
+    columns {
+      width {
+        # cat=introduction plus - top bar/150/100; type=options[1,2,3,4,5,6,7,8,9,10];   label=Width of left column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
+        left    = 3
+        # cat=introduction plus - top bar/150/101; type=options[1,2,3,4,5,6,7,8,9,10];   label=Width of center column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
+        center  = 6
+        # cat=introduction plus - top bar/150/102; type=options[1,2,3,4,5,6,7,8,9,10];   label=Width of right column:Value from 1 to 10. BE AWARE: the sum of all three columns must be 12!
+        right   = 3
+      }
+    }
+    folders {
+      # cat=introduction plus - top bar/150/200; type=int+;   label=Pid left content:The id of the folder with content for the left column.
+      left    =
+      # cat=introduction plus - top bar/150/201; type=int+;   label=Pid center content:The id of the folder with content for the center column.
+      center  =
+      # cat=introduction plus - top bar/150/202; type=int+;   label=Pid right content:The id of the folder with content for the right column.
+      right   =
+    }
+  }
+}
